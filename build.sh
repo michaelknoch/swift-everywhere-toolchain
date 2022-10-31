7871872cd9a467711d07e9cb5e4750b2800d4a71
Michael Knoch <info@michaelknoch.de>
2022-10-31 13:41:13 +0100

    update openSSL

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 export PATH="$ANDROID_NDK_PATH/toolchains/llvm/prebuilt/darwin-x86_64/bin:$PATH"
 
+export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+
+# not needed after intalling command line tools
+# export LIBRARY_PATH="$LIBRARY_PATH:/Applications/Xcode_14.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib"
+
 node main.js bootstrap
